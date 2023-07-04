09b4480a482dee80f34614ba38fef37b10648126
SooHyunKim <kshyunn12@gmail.com>
2023-07-04 17:29:04 +0900

    RunningTimeActivity almost finish

--- fatman/app/src/main/java/com/project/fat/MapsActivity.kt
+++ fatman/app/src/main/java/com/project/fat/MapsActivity.kt
@@ -1,7 +1,16 @@
 package com.project.fat
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,6 +24,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private val permission = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+    private val PERMISSION_FLAG = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +35,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -30,10 +42,34 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("location permission not allow" , "should allow the location permission")
+            Toast.makeText(this, "위치 권한이 필요한 기능 입니다.", Toast.LENGTH_SHORT).show()
+            ActivityCompat.requestPermissions(this, permission, PERMISSION_FLAG)
+        }
+        mMap.isMyLocationEnabled = true
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        setLocation(mMap)
+        val icon = setMarkerBitmap()
+    }
+
+    private fun setLocation(map: GoogleMap) {
+
+    }
+
+    private fun setMonsterMarker(map : GoogleMap, icon : Bitmap) {
+
+    }
+
+    private fun setMarkerBitmap() {
+
     }
 }
--- fatman/app/src/main/java/com/project/fat/RunningTimeActivity.kt
+++ fatman/app/src/main/java/com/project/fat/RunningTimeActivity.kt
@@ -2,10 +2,94 @@ package com.project.fat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.project.fat.databinding.ActivityRunningTimeBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import java.lang.Math.round
+import kotlin.random.Random
 
 class RunningTimeActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRunningTimeBinding
+    private var PLAY_BUTTON_PAUSE_STATE = false
+    private lateinit var timeJob: Job
+    private lateinit var distanceJob : Job
+    private var time = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_running_time)
+        binding = ActivityRunningTimeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        timeCoroutine(time)
+        distanceCoroutine()
+
+        binding.playBtn.setOnClickListener {
+            if(PLAY_BUTTON_PAUSE_STATE && timeJob.isCancelled){
+                timeCoroutine(time)
+                distanceCoroutine()
+                PLAY_BUTTON_PAUSE_STATE = false
+            }else if(!PLAY_BUTTON_PAUSE_STATE && !timeJob.isCancelled){
+                timeJob.cancel()
+                distanceJob.cancel()
+                PLAY_BUTTON_PAUSE_STATE = true
+            }else{
+                Log.d("timeCoroutine", "play button is not correct or timejob is not cancelled")
+                Toast.makeText(this, "스톱워치 문제 발생, 초기화합니다.", Toast.LENGTH_SHORT).show()
+                PLAY_BUTTON_PAUSE_STATE = false
+                timeJob.cancel()
+                distanceJob.cancel()
+                timeCoroutine(time)
+                distanceCoroutine()
+            }
+        }
+    }
+
+    private fun timeCalculate(time : Int) : String? {
+        var timeString : String? = null
+
+        if(time >= 0){
+            timeString = (time/60).toString() + ":" + if((time%60) < 10) {
+                "0" + (time%60).toString()
+            } else {
+                (time%60).toString()
+            }
+        }else{
+            Log.d("time", "time < 0")
+        }
+
+        return timeString
+    }
+
+    private fun timeCoroutine(t : Int) {
+        timeJob = lifecycleScope.launchWhenCreated {
+            var time = t
+            while(true){
+                val check = timeCalculate(time)
+                if(check.isNullOrBlank())
+                {
+                    time = 0
+                    this.cancel()
+                }else{
+                    binding.time.text = check
+                    delay(1000)
+                    time++
+                }
+            }
+        }
+    }
+
+    private fun distanceCoroutine() {
+        distanceJob = lifecycleScope.launchWhenCreated {
+            while(true)
+            {
+                //read distance
+                binding.kilometer.text = (round(Random.nextFloat()*1000) / 1000.0).toString()
+                delay(500)
+            }
+        }
     }
 }
