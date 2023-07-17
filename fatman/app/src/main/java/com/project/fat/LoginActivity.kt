4017f6c2b272f4c341757d638603a654be715326
haweeee <109846781+haweeee@users.noreply.github.com>
2023-07-18 02:51:12 +0900

    google login

--- fatman/app/src/main/java/com/project/fat/LoginActivity.kt
+++ fatman/app/src/main/java/com/project/fat/LoginActivity.kt
@@ -6,19 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.project.fat.BuildConfig.naver_client_id
 import com.project.fat.BuildConfig.kakaoLoginKey
 import com.project.fat.BuildConfig.naver_client_secret
+import com.project.fat.BuildConfig.google_client_id
 
-import com.facebook.AccessToken
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.FacebookSdk
-import com.facebook.login.LoginManager
-import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.auth.model.OAuthToken
@@ -37,10 +41,11 @@ import java.util.Arrays
 
 class LoginActivity : AppCompatActivity() {
     lateinit var loginBinding: ActivityLoginBinding
-    private lateinit var auth: FirebaseAuth
     lateinit var user_id: String
-    lateinit var callbackManager: CallbackManager
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var startGoogleLoginForResult : ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         loginBinding = ActivityLoginBinding.inflate(layoutInflater)
 
@@ -51,6 +56,7 @@ class LoginActivity : AppCompatActivity() {
         NaverIdLoginSDK.initialize(baseContext,naver_client_id,naver_client_secret,"네이버 로그인")
         //Log.d(TAG, "keyhash ${Utility.getKeyHash(this)}")
         //DP1zQOa5mjnTte/hgypxWr7Llig=
+        getGoogleClient()
 
 
 
@@ -60,19 +66,17 @@ class LoginActivity : AppCompatActivity() {
         loginBinding.naverLogin.setOnClickListener{
             naverLogin()
         }
-
+        loginBinding.googleLogin.setOnClickListener {
+            googleLogin()
+        }
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
-        // Pass the activity result back to the Facebook SDK
-        callbackManager.onActivityResult(requestCode, resultCode, data)
-    }
     override fun onDestroy() {
         super.onDestroy()
         kakaoLogout()
         kakaoUnlink()
         NaverIdLoginSDK.logout()
+        googleLogout()
 
     }
 
@@ -129,8 +133,8 @@ class LoginActivity : AppCompatActivity() {
                 Log.e(TAG, "사용자 정보 요청 실패", error)
             }
             else if (user != null) {
-                Log.i(TAG, "사용자 정보 요청 성공" +
-                        "\n회원번호: ${user.id}" +
+                Log.i(TAG, "카카오 로그인 사용자 정보 요청 성공" +
+                        "\n회원아이디: ${user.id}" +
                         "\n이메일: ${user.kakaoAccount?.email}" +
                         "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
                         "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
@@ -168,7 +172,9 @@ class LoginActivity : AppCompatActivity() {
                 NidOAuthLogin().callProfileApi(object : NidProfileCallback<NidProfileResponse> {
                     override fun onSuccess(result: NidProfileResponse) {
                         user_id = result.profile?.id.toString()
-                        Log.e(TAG, "네이버 로그인한 유저 정보 - 아이디 : $user_id")
+                        Log.e(TAG, "네이버 로그인한 유저 정보 - 아이디 : $user_id" +
+                                "\n이메일: ${result.profile?.email}" +
+                                "\n닉네임: ${result.profile?.nickname}")
 
                         val accessToken = NaverIdLoginSDK.getAccessToken()
 
@@ -205,6 +211,74 @@ class LoginActivity : AppCompatActivity() {
         NaverIdLoginSDK.authenticate(this, oauthLoginCallback)
     }
 
+    //google login
+    private fun googleLogin() {
+        //GoogleSignInClient.signOut()
 
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startGoogleLoginForResult.launch(signInIntent)
+    }
+    private fun getGoogleClient() {
+        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            //.requestScopes(Scope("https://www.googleapis.com/auth/pubsub"))
+            .requestServerAuthCode(google_client_id) // string 파일에 저장해둔 client id 를 이용해 server authcode를 요청한다.
+            .requestEmail() // 이메일도 요청할 수 있다.
+            .build()
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOption)
+
+        startGoogleLoginForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                if (result.resultCode == RESULT_OK) {
+                    result.data?.let { data ->
+
+                        val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+                        try {
+                            // Google Sign In was successful, authenticate with Firebase, 사용자 정보 가져오기
+                            val account = task.getResult(ApiException::class.java)!!
+
+                            user_id = account.id.toString()
+                            Log.d(TAG, "카카오 로그인 사용자 정보 요청 성공" +
+                                    "\n회원아이디: ${user_id}" +
+                                    "\n이메일: ${account.email}" +
+                                    "\n닉네임: ${account.familyName + account.givenName} ")
+                            Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                            firebaseAuthWithGoogle(account.idToken!!)
+                        } catch (e: ApiException) {
+                            // Google Sign In failed, update UI appropriately
+                            Log.w(TAG, "Google sign in failed", e)
+                        }
+                    }
+                    // Google Login Success
+                } else {
+                    Log.e(TAG, "Google Result Error ${result}")
+                }
+            }
+
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+
+                }
+            }
+    }
+   private fun googleLogout(){
+       mGoogleSignInClient.signOut()
+           .addOnCompleteListener(this) {
+               // 로그아웃 성공시 실행
+               // 로그아웃 이후의 이벤트들(토스트 메세지, 화면 종료)을 여기서 수행하면 됨
+           }
+   }
 }
