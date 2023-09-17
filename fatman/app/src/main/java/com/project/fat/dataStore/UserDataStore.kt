9e7c716ad2a13233893de76f211db478c8fe2917
kim suhyun <114320282+ksh-g001@users.noreply.github.com>
2023-09-17 21:15:50 +0900

    Update UserDataStore.kt
    되돌렸습니다.

--- fatman/app/src/main/java/com/project/fat/dataStore/UserDataStore.kt
+++ fatman/app/src/main/java/com/project/fat/dataStore/UserDataStore.kt
@@ -1,16 +1,14 @@
 package com.project.fat.dataStore
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.core.DataStore
-import androidx.datastore.dataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import com.project.fat.dataStore.UserDataStore.dataStore
-import kotlinx.coroutines.flow.Flow
+import com.project.fat.data.store.StoreAvata
 
 object UserDataStore{
     val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user")
@@ -21,6 +19,19 @@ object UserDataStore{
     val SELECTED_FATMAN_IMAGE = stringPreferencesKey("selected_fatman_image")
     val ACCESS_TOKEN = stringPreferencesKey("access_token")
     val REFRESH_TOKEN = stringPreferencesKey("refresh_token")
-    val TODAY_MONSTER_NUM = intPreferencesKey("today_monster_num")
+
+    suspend fun saveSelectedFatman(context: Context, selectedFatman : StoreAvata){
+        try {
+            context.dataStore.edit {
+                Log.d("saveSelectedFatman", "edit start")
+                it[SELECTED_FATMAN_ID] = selectedFatman.id
+                Log.d("saveSelectedFatman", "set id")
+                it[SELECTED_FATMAN_IMAGE] = selectedFatman.fatmanImage
+                Log.d("saveSelectedFatman", "edit end")
+            }
+        }catch (e: Exception){
+            Log.e("saveSelectedFatman", "error : ${e.message}", e)
+        }
+    }
 }
 
